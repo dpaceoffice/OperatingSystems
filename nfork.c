61ad5fb6592eb726f1242360fb67efaffea6fc39
dpaceoffice <dpaceoffice@gmail.com>
2021-09-19 18:13:06 -0500

    deb

--- nfork.c
+++ nfork.c
@@ -4,13 +4,13 @@
 int main(int argc, char *argv[])
 {
     int n = (int) argv[1];        
-
+    printf("%d", n);
     for(int i = 1; i < n; i++) {
         if(fork() == 0) {
             printf("PID: %d, Parent PID: %d\n", getpid(), getppid());
             exit(0);
         }
     }
-
+    waitpid();
     return 0;
 }
