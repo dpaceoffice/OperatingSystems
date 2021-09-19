4be39ddeb8018212565e792888fb7e8f31a02544
dpaceoffice <dpaceoffice@gmail.com>
2021-09-19 18:20:47 -0500

    testst

--- nfork.c
+++ nfork.c
@@ -4,13 +4,12 @@
 int main(int argc, char *argv[])
 {
     int n = strtol(argv[1], NULL, 10);        
-    printf("%d", n);
     for(int i = 1; i < n; i++) {
         if(fork() == 0) {
             printf("PID: %d, Parent PID: %d\n", getpid(), getppid());
             exit(0);
+        } else {
+            waitpid();
         }
     }
-    waitpid();
-    exit(0);
 }
