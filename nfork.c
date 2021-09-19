d84e489061856cbe930cad2c912be7ea622a4159
dpaceoffice <dpaceoffice@gmail.com>
2021-09-19 17:45:22 -0500

    Script

---
+++ nfork.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int n = (int) argv[1];        
+
+    for(int i = 1; i < n; i++) {
+        if(fork() == 0) {
+            printf("Parent PID: %d, PID: %d", getpid(), getppid());
+            exit(0);
+        }
+    }
+    waitpid();
+    return 0;
+}
